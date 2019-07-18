d2d1052be45c748362edfdadfc930cec6db861e9
gkrdud0822 <hakyoung0822@gmail.com>
2019-07-18 17:49:46 +0900

    시간선택, 인원선택 추가

--- src/com/movie/panel/ReservationPanel.java
+++ src/com/movie/panel/ReservationPanel.java
@@ -3,6 +3,7 @@ package com.movie.panel;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.util.Vector;
 
 import javax.swing.BorderFactory;
@@ -25,18 +26,21 @@ public class ReservationPanel extends JPanel{
 	Vector<String> cinemaL=new Vector<>();
 	
 	/* 영화시간 선택 */
-	JPanel time_room=new JPanel();
-	JPanel time=new JPanel();
 	JPanel room=new JPanel();
-	JLabel timeLabel=new JLabel();
-	JLabel roomLabel=new JLabel();
+	JPanel time01=new JPanel();
+	JPanel time02=new JPanel();
+	TimeButton timeButton01=new TimeButton();
+	TimeButton timeButton02=new TimeButton();
+	JLabel roomLabel01,roomLabel02;
 	
 	/* 날짜 선택 */
 	JPanel days=new JPanel();
 	
 	/* 인원 선택 */
 	JPanel people=new JPanel();
-	
+	PeopleButton peopleB01=new PeopleButton();
+	PeopleButton peopleB02=new PeopleButton();
+	JLabel adult,child;
 	/* 최종정보&버튼 */
 	JPanel info=new JPanel();
 	
@@ -78,18 +82,54 @@ public class ReservationPanel extends JPanel{
 				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		scroll02.setPreferredSize(new Dimension(300,300));
 		cinema.add(scroll02);
+		/* 영화시간 선택 */
+		time01.add(timeButton01);
+		time02.add(timeButton02);
+		/* 상영관 설정 */
+		roomLabel01=new JLabel("   1관");
+		roomLabel02=new JLabel("   2관");
+		room.setLayout(new GridLayout(0,1)); // 상영관 정보 배치관리자 설정
+		/* 상영관정보+시간버튼 추가 */
+		room.add(roomLabel01); room.add(time01);
+		room.add(roomLabel02); room.add(time02);
+		/* 인원수 선택 패널 */
+		people.setLayout(new GridLayout(0,1));
+		adult=new JLabel("   성인");
+		child=new JLabel("   청소년");
+		people.add(adult); people.add(peopleB01);
+		people.add(child); people.add(peopleB02);
+		
 		/* 메인패널에 보조패널 추가 */
 		add(movie);	add(cinema);
-		add(time_room);	add(days);
+		add(room);	add(days);
 		add(people); add(info);
 	}//cons ReservationPanel()
+	
+	/* 상용화 버튼 클래스 생성 */
 	class TimeButton extends JPanel{
-		JButton[] button=new JButton[8];
+		JButton[] button=new JButton[4];
 		
 		public TimeButton() {
-			for(int i=0;i<8;i++) {
-				button[i].
-			}
-		}
+			for(int i=0;i<4;i++) {
+				button[i]=new JButton();
+				button[i].setPreferredSize(new Dimension(70,25));
+				add(button[i]);
+			}//for
+			button[0].setText("10:00");
+			button[1].setText("13:00");
+			button[2].setText("16:00");
+			button[3].setText("19:00");
+		}//cons TimeButton()
 	}//TimeButton inner class
+	class PeopleButton extends JPanel{
+		JButton[] button=new JButton[8];
+		
+		public PeopleButton() {
+			for(int i=0;i<8;i++) {
+				button[i]=new JButton((i+1)+"");
+				button[i].setPreferredSize(new Dimension(50,20));
+				add(button[i]);
+				}//for
+		}
+	}//PeoPleButton inner class
 }//Reservation class
