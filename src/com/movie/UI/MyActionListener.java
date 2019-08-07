f4696805fce751919a7a23bee4bad2066f3ce646
midnight9040 <midnight9040@gmail.com>
2019-08-07 10:17:09 +0900

    동환 회원가입창 리뉴얼

--- src/com/movie/UI/MyActionListener.java
+++ src/com/movie/UI/MyActionListener.java
@@ -3,23 +3,94 @@ package com.movie.UI;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.regex.Pattern;
 
 import javax.swing.BorderFactory;
 
-import com.movie.frame.SignUP;
 import com.movie.main.AppManager;
 
 public class MyActionListener {
 	MainUI mainUi=new MainUI();
 	LoginPage loginP;
-	SignUP signUp;
+	SignUpFrame signUp;
 	LoginActionL logL=new LoginActionL();
 	MainActionL mainL=new MainActionL();
+	SignupActionL signupL=new SignupActionL();
 
 	public MyActionListener() {
 		AppManager.getInstance().setMyListener(this);
 	}//cons MyActionListener()
+	
+	class SignupActionL implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource()==signUp.check_idB) {
+				if((signUp.idTF.getText().length()<6 || signUp.idTF.getText().length()>16) || 
+						!(Pattern.matches("^[a-z]+[a-z0-9]*$",signUp.idTF.getText()))) {
+					signUp.error_idL.setText("아이디는 영문소문자로 시작하는 6~16자리 영문 소문자와 숫자로만 입력하세요!");
+				}else {//아이디 경고문 출력
+					/**DB에서 id search=>비교 if절 추가해야됨**/		
+					signUp.check_idB.setEnabled(false);
+				}
+			}//아이디 중복체크 버튼 선택시
 
+			if(e.getSource()==signUp.check_emailB) {
+				
+			}//이메일 인증번호 버튼 선택시
+
+			if(e.getSource()==signUp.confirmB) {
+				signUp.error_idL.setText("");   //회원가입 버튼 선택시 경고문 초기화
+				signUp.error_passL.setText("");
+				signUp.error_nameL.setText("");
+				signUp.error_birthL.setText("");
+				signUp.error_emailL.setText("");
+				
+				if(signUp.check_idB.isEnabled()==true) { //중복검색 안했을때(버튼 활성화되어있을때)
+					signUp.error_idL.setText("아이디를 입력한 후 중복확인을 해주세요!");
+					signUp.idTF.requestFocus();
+				}//아이디 중복검색 버튼 안눌렀을 때
+				
+				if(signUp.passPF.getText().length()<6 || signUp.passPF.getText().length()>16 || 
+						!(Pattern.matches("^[a-z0-9]*$",signUp.passPF.getText()))) {
+					signUp.error_passL.setText("비밀번호는 6~16사이 영문소문자와 숫자로만 입력하세요!");
+					signUp.passPF.setText("");
+					signUp.passrePF.setText("");
+					signUp.passPF.requestFocus();
+				}//비밀번호 입력값이 올바르지 않을 때
+				
+				if(!signUp.passPF.getText().equals(signUp.passrePF.getText())) {
+					signUp.error_passL.setText("비번이 일치하지 않습니다!");
+					signUp.passPF.setText(""); 
+					signUp.passrePF.setText("");
+					signUp.passPF.requestFocus();
+				}//비밀번호 입력값과 재입력값이 다른경우
+				
+				if(signUp.nameTF.getText().trim().equals("")) {
+					signUp.error_nameL.setText("이름을 입력하세요!");
+					signUp.nameTF.requestFocus();
+				}//이름란이 공백일 때
+				
+				if(signUp.yearTF.getText().trim().equals("") || signUp.monthTF.getText().trim().equals("") ||
+						signUp.dateTF.getText().trim().equals("") ||                     //생년월일에 공백이 있을 때 
+						(!(Pattern.matches("^[0-9]*$", signUp.yearTF.getText()))) ||     //연 텍스트필드에 숫자가 아닌것이 있을때
+						(!(Pattern.matches("^[0-9]*$", signUp.monthTF.getText()))) ||    //월 텍스트필드에 숫자가 아닌것이 있을때
+						(!(Pattern.matches("^[0-9]*$", signUp.dateTF.getText())))) {     //일 텍스트필드에 숫자가 아닌것이 있을때
+					signUp.error_birthL.setText("생년월일을 정확히 입력해 주세요!");
+					signUp.yearTF.requestFocus();
+				}//생년월일 란에 문제가 있을 때
+				
+				if(signUp.emailTF.getText().trim().equals("")) {
+					signUp.error_emailL.setText("이메일을 입력하세요!");
+					signUp.emailTF.requestFocus();
+				}//이메일 란이 공백일 때
+			}else {
+				signUp.dialog.showMessageDialog(signUp, "회원가입이 되셨습니다!", "안내", signUp.dialog.CLOSED_OPTION);
+				signUp.dispose();
+			}//가입하기 버튼 눌렀을 때
+			
+		}//aP()
+	}//SignupActionL inner class
+	
 	class LoginActionL implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e) {
@@ -56,7 +127,8 @@ public class MyActionListener {
 				}// 아이디 미입력시
 			}// 로그인버튼 클릭시
 			if(obj==loginP.addMem) {
-				signUp = new SignUP();
+				signUp = new SignUpFrame();
+				signupListenerSet();
 			}
 		}//aP()
 	}// LoginActionL inner class
@@ -108,7 +180,8 @@ public class MyActionListener {
 				loginP = new LoginPage();
 				loginListenerSet();
 			}else if(obj==mainUi.registB) {
-				signUp = new SignUP();
+				signUp = new SignUpFrame();
+				signupListenerSet();
 			}// 메인페이지 로그인버튼/회원가입 버튼 선택 이벤트
 		}//ap()
 	}// MainActionL class
@@ -117,5 +190,8 @@ public class MyActionListener {
 	}
 	public void mainListenerSet() {
 		mainUi.addMainListener(mainL);
+	}
+	public void signupListenerSet() {
+		signUp.addSignupListener(signupL);
 	}
 }//MyActionListener class
--- src/com/movie/main/AppManager.java
+++ src/com/movie/main/AppManager.java
@@ -4,15 +4,15 @@ import com.movie.DAO.DAOManager;
 import com.movie.UI.LoginPage;
 import com.movie.UI.MainUI;
 import com.movie.UI.MyActionListener;
+import com.movie.UI.SignUpFrame;
 import com.movie.VO.DataManager;
-import com.movie.frame.SignUP;
 
 public class AppManager {
 	private static AppManager appManager=null;
 	private MyActionListener myListener;
 	private MainUI mainUi;
 	private LoginPage login_page;
-	private SignUP signUp;
+	private SignUpFrame signUp;
 	private DAOManager daoManager;
 	private DataManager dataManager;
 	
@@ -26,7 +26,7 @@ public class AppManager {
 
 	//******************************get 메서드*****************************//
 	public LoginPage getLogin() {		return login_page;		}
-	public SignUP getSignUp() {		return signUp;		}
+	public SignUpFrame getSignUpFrame() {		return signUp;		}
 	public MainUI getMainUi() {		return mainUi;  	}
 	public MyActionListener getMyListener() {		return myListener;		}
 	public DAOManager getDAOManager() {	return daoManager;	}
@@ -34,7 +34,7 @@ public class AppManager {
 	
 	//******************************set 메서드*****************************//
 	public void setLogin(LoginPage login) {		this.login_page = login;		}
-	public void setSignUp(SignUP signUp) {  	this.signUp = signUp; 		}
+	public void setSignUpFrame(SignUpFrame signUp) {  	this.signUp = signUp; 		}
 	public void setMainUi(MainUI mainUi) {		this.mainUi = mainUi;		}
 	public void setMyListener(MyActionListener myListener) {	this.myListener = myListener;	}
 	public void setDAOManager(DAOManager daoManager) { this.daoManager = daoManager;	}
