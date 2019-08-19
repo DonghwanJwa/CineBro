f1024c066ac9cb4fcd1a1162de0c34f7bc8a04a8
midnight9040 <midnight9040@gmail.com>
2019-08-20 00:11:25 +0900

    이메일 인증 클래스파일 삭제->MemberDAO에 넣어버림 , 회원가입시 이메일 인증 기능 넣었음

--- SQL/day_seat.sql
+++ SQL/day_seat.sql
@@ -12,16 +12,16 @@ declare
 	dat NUMBER(20);		--0823~0825에서 3~5만
 	scr NUMBER(20);		--관 번호
 	tco NUMBER(20);		--회차 (08231~08235에서 1~5만)
-	low NUMBER(20);		--좌석 행
+	row NUMBER(20);		--좌석 행
 	col NUMBER(20);		--좌석 열
 begin
 for dat in 3..5 LOOP
 	for scr in 1..12 LOOP
 		for tco in 1..5 LOOP
-			for low in 65..76 LOOP
+			for row in 65..76 LOOP
 				for col in 1..20 LOOP
 					insert into day_seat (seat_status,seat_Num,screen,time_code)
-					values (0,scr||'관'||CHR(low)||col,scr||'관',scr||'관082'||dat||tco);
+					values (0,scr||'관'||CHR(row)||col,scr||'관',scr||'관082'||dat||tco);
 				end LOOP;
 			end LOOP;
 		end LOOP;
