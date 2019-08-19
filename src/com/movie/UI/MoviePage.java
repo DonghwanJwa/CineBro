5eaf51421d591eb00554eb5ef894afc3542fdbd9
노광훈 <undun01@gmail.com>
2019-08-19 09:05:42 +0900

    Revert "영화 정보페이지 디자인 변경"
    This reverts commit 8d04f844d8122d6986b02dfe26a341e093b2bda1.

--- src/com/movie/UI/MoviePage.java
+++ src/com/movie/UI/MoviePage.java
@@ -45,32 +45,34 @@ public class MoviePage extends JPanel implements ActionListener{
 	//	이미지 만들기	//
 	ImageIcon img =new ImageIcon();
 	//		폰트 객체 만들기		// 
-	Font f1=new Font("맑은 고딕",Font.BOLD,40);
+	Font f1=new Font("함초롱바탕",Font.BOLD,30);
 	Font f2=new Font("함초롱바탕",Font.PLAIN,15);//보통글씨로
 
 	//	 패널 생성	 //
 	public MoviePage() {
 
-		setOpaque(false);
 		posterB =new JButton[nlist.size()]; 
-		contents =new JPanel();// 메인 패널 
+		movieList =new JPanel();// 메인 패널
+		contents =new JPanel();//포스터 및 영화 정보를 모두 넣을 패널
 
 		//		패널 설정		//
-		setBorder(BorderFactory.createEmptyBorder(60,70,60,80));//	패널 공간 여백 만듦
-		setPreferredSize(new Dimension(1550,860));//	크기지정
-		contents.setBackground(Color.DARK_GRAY);// 패널 색 지정
-//		setOpaque(false);	//패널을 투명하게
+		movieList.setBorder(BorderFactory.createEmptyBorder(60,70,60,80));//	패널 공간 여백 만듦
+		movieList.setPreferredSize(new Dimension(1400,860));//	크기지정
+		movieList.setBackground(Color.cyan);//	패널 색 지정
 
+		contents.setBackground(Color.lightGray);//패널 색 지정
 		//	스크롤바 사용을 위해 증가하는 방식으로 패널 크기지정	//
-		contents.setPreferredSize(new Dimension(1400,270*(nlist.size()/2+1/2)));
-		//		패널 정렬		//
-		setLayout(new BorderLayout());
+		contents.setPreferredSize(new Dimension(1200,249*(nlist.size()/2+1/2)));
+		movieList.setLayout(new BorderLayout());
 		contents.setLayout(new FlowLayout(FlowLayout.LEFT));
+
 		/**내부 패널에 작은 패널들 추가**/
 		for(int i=0; i<nlist.size(); i++) {
-			add(MP(i));
+			movieList.add(MP(i));
 		}
-		add(contents,BorderLayout.CENTER);
+		movieList.add(contents,BorderLayout.CENTER);
+
+		add(movieList);
 
 		/**------------------------------DB이후------------------------------------**/
 		//	스크롤 만들기		//
@@ -78,34 +80,29 @@ public class MoviePage extends JPanel implements ActionListener{
 				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		//	내부 패널에 스크롤 적용 후 상하스크롤 항상 보이게, 좌우스크롤 항상 숨김
 		scroll.getVerticalScrollBar().setUnitIncrement(16);
-
 		//		내부패널에 스크롤 적용	//
-		add(scroll,BorderLayout.EAST);
+		movieList.add(scroll,BorderLayout.EAST);
 
 	}//생성자
 
 	//	패널 정리할 메서드	생성//
 	public Component MP (int index) {
 
-		movieP =new JPanel();	//	영화 포스터 버튼을 넣을 패널
-		movieLine =new JPanel();//	영화정보를 넣을 패널
-		movieList =new JPanel();//	위 두개의 정보가 모두 들어가는 패널
+		movieP =new JPanel();
+		movieLine =new JPanel();
 		//		패널  크기 설정		//
-		movieP.setPreferredSize(new Dimension(270,255));
-		movieLine.setPreferredSize(new Dimension(470,255));
-		movieList.setPreferredSize(new Dimension(670,260));
-		// 		패널 배경 색 지정		//
-		movieP.setBackground(Color.BLACK);
-		movieLine.setBackground(Color.BLACK);
-		movieList.setBackground(Color.BLACK);
+		movieP.setPreferredSize(new Dimension(190,233));
+		movieLine.setPreferredSize(new Dimension(390,233));
 
 		//	영화정보에 사용될 패널,라벨,버튼(포스터 이미지) 만들기		//
 		//		버튼에 넣을 이미지 생성		//
+		movieLine.setLayout(new GridLayout(0,1));
 		ImageIcon preImg = new ImageIcon("pic/"+nlist.get(index).getImg());//포스터 넣을 이미지 객체 생성(배열 값)
 		Image originImg = preImg.getImage();//ImageIcon을 Image로 전환
-		Image changeImg = originImg.getScaledInstance(155,250,java.awt.Image.SCALE_SMOOTH);
+		Image changeImg = originImg.getScaledInstance(150,220,java.awt.Image.SCALE_SMOOTH);
 		//이미지 사이즈 가로130,세로180,이미지를 스무스하게
 		ImageIcon poster = new ImageIcon(changeImg);//Image로 ImageIcon 지정
+
 		//			버튼 객체 생성			//
 		posterB[index] = new JButton(poster);//포스터 DB
 		//			라벨 객체생성			//
@@ -119,12 +116,8 @@ public class MoviePage extends JPanel implements ActionListener{
 		//		폰트 지정		//
 		movieName.setFont(f1);	genre.setFont(f2);	age.setFont(f2);
 		director.setFont(f2);	actor.setFont(f2);
-		// 		글자 색 지정		//
-		movieName.setForeground(Color.white);	genre.setForeground(Color.white);	
-		age.setForeground(Color.white);		director.setForeground(Color.white);	
-		actor.setForeground(Color.white);
 		// 		버튼 설정		//
-		posterB[index].setBackground(Color.black);
+		posterB[index].setBackground(Color.GRAY);
 		posterB[index].setFocusPainted(false);//버튼 선택시 테두리 사용(안함)
 		posterB[index].setBorderPainted(false);//버튼 테두리 사용(안함)
 		posterB[index].setOpaque(true);//투명도를 (투명하게)
@@ -137,15 +130,11 @@ public class MoviePage extends JPanel implements ActionListener{
 		movieP.add(actor);
 		movieLine.add(movieP);
 		//			정보패널 정렬		//
-		movieLine.setLayout(new GridLayout(0,1));
 		movieP.setLayout(new GridLayout(0,1));
-		//		패널 넣기		//
-		movieList.add(posterB[index]);
-		movieList.add(movieLine);
 
-		contents.add(movieList);
-
-		return contents;//contents 패널에 리턴
+		contents.add(posterB[index]);
+		contents.add(movieLine);
+		return contents;
 	}//MP()메서드
 	@Override
 	public void actionPerformed(ActionEvent e) {
